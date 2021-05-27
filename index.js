788f18a0e392ed510a851c3256ca90c2e11e780e
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-05-27 16:51:10 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,20 +7,20 @@ async function getBeers() {
         const beers = document.querySelector(".beers");
         const beerDiv = document.createElement("div");
         let beerHTML = '';
-        beerHTML += 
-        `
-        <div class='beer-wrapper card'>
-            <div class='beer'>
-                <img class='beer__img' src="${}">
-                <h3>${beer.name}</h3>
-                <span class='beer__info'>
-                    <span>ABV: ${farts}%</span>
-                    <span>IBU: ${burps}</span>
-                </span>
-            </div>
-        </div>
-       `;
-        beersDiv.innerHTML = beerHTML;
+        beerHTML += `<h3>${beer.name}</h3>`;
+    //     `
+    //     <div class='beer-wrapper card'>
+    //         <div class='beer'>
+    //             <img class='beer__img' src="${}">
+    //             <h3>${beer.name}</h3>
+    //             <span class='beer__info'>
+    //                 <span>ABV: ${farts}%</span>
+    //                 <span>IBU: ${burps}</span>
+    //             </span>
+    //         </div>
+    //     </div>
+    //    `;
+        beerDiv.innerHTML = beerHTML;
         beers.appendChild(beerDiv);
     })
 }
