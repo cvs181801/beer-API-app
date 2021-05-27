1f206546d4fcae069490032d81afde74603af833
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-05-27 16:57:22 -0700

    Update index.js

--- index.js
+++ index.js
@@ -11,15 +11,15 @@ async function getBeers() {
         `
         <div class='beer-wrapper card'>
             <div class='beer'>
-                <img class='beer__img' src="${}">
+                <img class='beer__img' src="${beer.image_url}">
                 <h3>${beer.name}</h3>
                 <span class='beer__info'>
-                    <span>ABV: ${farts}%</span>
-                    <span>IBU: ${burps}</span>
+                    <span>ABV: ${beer.abv}%</span>
+                    <span>IBU: ${beer.ibu}</span>
                 </span>
             </div>
         </div>
-       `;
+       `; 
         beerDiv.innerHTML = beerHTML;
         beers.appendChild(beerDiv);
     })
