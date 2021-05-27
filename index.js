9836ea5fc5e9e73df58dd5337f210a672f7e18aa
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-05-27 16:47:02 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,19 @@ async function getBeers() {
     beerResponseJson.forEach((beer) => {
         const beers = document.querySelector(".beers");
         const beerDiv = document.createElement("div");
-        beerDiv.innerText = `${beer.name}`;
+        beerDiv.innerHTML = 
+        `
+        <div class='beer-wrapper card'>
+            <div class='beer'>
+                <img class='beer__img' src="${}">
+                <h3>${beer.name}</h3>
+                <span class='beer__info'>
+                    <span>ABV: ${farts}%</span>
+                    <span>IBU: ${burps}</span>
+                </span>
+            </div>
+        </div>
+       `;
         beers.appendChild(beerDiv);
     })
 }
